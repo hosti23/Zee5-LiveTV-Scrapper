9bed4908c42e2ea72ae1d883902e1b3d9ca84a58
hosti23 <82031124+hosti23@users.noreply.github.com>
2021-05-26 00:58:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 $input = $_GET["c"];
 if (!$input){
-   exit("<h3>Channel ID not found <br><br>You Entered Worng ID or Not Entered ID Here</h3><br><h4> Use Correct Format ➸ <code>https://zee.avipatilpro.repl.co/?c=CHANNEL_ID_HERE</code> <br><br><br> <h4> ➤ Created by <a href='https://github.com/avipatilpro'>Avi Patil</a></h4> ");
+   exit("<h3>Channel ID not found <br><br>You Entered Worng ID or Not Entered ID Here</h3><br><h4> Use Correct Format ➸ <code>https://payzeea.herokuapp.com/?c=CHANNEL_ID_HERE</code> <br><br><br> <h4> ➤ Created by <a href='https://github.com/avipatilpro'>Avi Patil</a></h4> ");
   
 }
 $channel_meta = JsonfromURI("https://catalogapi.zee5.com/v1/channel/${input}");
@@ -13,9 +13,8 @@ $stream_url = $channel_meta->stream_url_hls;
 $tok_json = JsonfromURI("https://useraction.zee5.com/token/live.php");
 $video_token = $tok_json->video_token;
 $m3u8 = $stream_url.$video_token;
-echo $m3u8;
-// header("Location: $m3u8"); --> For Direct Play
 
+header("Location: $m3u8"); 
 function JsonfromURI($url) {
    $resp = file_get_contents($url);       
    return json_decode($resp);
